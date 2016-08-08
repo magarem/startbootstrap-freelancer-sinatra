10008b34f1270afc21db31abf6ce74fba672b78e
marcelo <contato@magaweb.com.br>
2016-08-08 16:57:38 -0300

    v0.8

--- public/js/angular_script.js
+++ public/js/angular_script.js
@@ -456,7 +456,7 @@ mod.controller('MyFormCtrl', ['$scope', '$rootScope', 'Upload', '$timeout', '$ht
         }, function (evt) {
             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
             console.log('---progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
-            $rootScope.$emit("ImgChange",file.name, index, siteNome);
+            if (progressPercentage == '100') {$rootScope.$emit("ImgChange",file.name, index, siteNome);}
         });
         
         
