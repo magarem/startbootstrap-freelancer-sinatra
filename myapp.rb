85d746d308b4a459395e353ac1eed1ecf095f880
marcelo a magalhaes <contato@magaweb.com.br>
2018-01-04 00:07:49 -0200

    freebds version

--- myapp.rb
+++ myapp.rb
@@ -13,7 +13,8 @@ require "aescrypt"
 require 'open-uri'
 require 'video_thumb'
 require 'cloudinary'
-require 'URLcrypt'
+#require 'URLcrypt'
+require 'openssl'
 
 set :public_folder, 'public'
 set :session_secret, "328479283uf923fu8932fu923uf9832f23f232"
@@ -54,6 +55,23 @@ configure do
   set :public_folder, Proc.new { File.join(root, "public") }
 end
 helpers do
+  class String
+    def encrypt(key)
+      cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').encrypt
+      cipher.key = Digest::SHA1.hexdigest key
+      s = cipher.update(self) + cipher.final
+
+      s.unpack('H*')[0].upcase
+    end
+
+    def decrypt(key)
+      cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').decrypt
+      cipher.key = Digest::SHA1.hexdigest key
+      s = [self].pack("H*").unpack("C*").pack("c*")
+
+      cipher.update(s) + cipher.final
+    end
+  end
   def h(text)
     Rack::Utils.escape_html(text)
   end
@@ -106,6 +124,17 @@ end
 #
 #  /adm
 #
+get '/a' do
+
+  puts plain = 'fidelização compĺeta da macacheira doida'           # confidential
+  puts key = 'rrrrrr'                   # secret
+  puts cipher = plain.encrypt(key)      # 5C6D4C5FAFFCF09F271E01C5A132BE89
+
+  #puts cipher.decrypt('guess')          # raises OpenSSL::Cipher::CipherError
+  puts cipher.decrypt(key)              # confidential
+
+end
+
 get '/adm' do
   redirect "http://#{@url}/site/index.html?cmd=login&site=#{@site_nome}"
 end
@@ -211,10 +240,11 @@ post "/site_new" do
 
   # encrypt and encode with 256-bit AES
   # one-time setup, set this to a securely random key with at least 256 bits, see below
-  URLcrypt.key = password
+  #URLcrypt.key = password
 
   # now encrypt!
-  encrypted_data = URLcrypt.encrypt(chave)
+  #encrypted_data = URLcrypt.encrypt(chave)
+  encrypted_data = chave.encrypt(password)
 
   puts "encrypted_data: key=#{encrypted_data}"
 
@@ -253,7 +283,8 @@ get "/siteNewDo" do
   # encrypt and encode with 256-bit AES
   # one-time setup, set this to a securely random key with at least 256 bits, see below
   URLcrypt.key = password
-  decrypted = URLcrypt.decrypt(chave)
+  #decrypted = URLcrypt.decrypt(chave)
+  decrypted = chave.decrypt(password)
 
   emailParams = decrypted.split("/")
   verifica = emailParams[0]
